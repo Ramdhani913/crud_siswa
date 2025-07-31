15e431a24972139358ce700f645d53a502a3321c
Ramdhani913 <eramdhani479@gmail.com>
2025-07-31 01:57:41 -0700

    3rd

---
+++ resources/views/siswa/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Beranda</h1>
+    <p>List Data Siswa</p>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>photo</th>
+                <th>nama</th>
+                <th>NISN</th>
+                <th>alamat</th>
+                <th>option</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>photo.jpg</td>
+                <td>Mulyadi</td>
+                <td>913</td>
+                <td>Jl. jalan</td>
+                <td>
+                    <a href="">detail</a>|
+                    <a href="">edit</a>|
+                    <a href="">hapus</a>|
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <br>
+    <a href="/siswa/create"><button>Tambah Data Siswa</button></a>
+</body>
+</html>
